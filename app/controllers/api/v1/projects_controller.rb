64ef5045a324de86db51004173fae29586bf5cc9
Faith Gicheha <fwgicheh@gmail.com>
2020-10-10 16:37:42 -0700

    add index method

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,9 +1,33 @@
 class Api::V1::ProjectsController < ApplicationController
   
   def index
-    categories = Category.all
+    projects = Project.all
     # render json: categories
-    render json: CategorySerializer.new(projects)
+    render json: ProjectSerializer.new(projects)
   end
+
+#    def create 
+#     # /task_params check if there is a new project name 
+#     # /if there is a new project name, create project and get the id 
+#     # /task params project_id = created project_id 
+
+#     # if new
+#     puts task_params
+#     byebug
+#     task = Task.new(task_params)
+#     # byebug
+#     if task.save
+#       render json: TaskSerializer.new(task), status: :accepted
+#       # render json: task, status: :accepted
+
+#     else 
+#       render json: {errors: task.errors.full_messages}, status: :unprocessable_entity
+#     end
+#   end 
+
+# private
+#   def task_params 
+#     params.require(:task).permit(:title, :deadline, :creator, :completed, :project_id)
+#   end
   
 end
