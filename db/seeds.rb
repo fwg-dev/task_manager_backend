34cc2f7bdecfd7b05ffffe3455520d230d08d231
Faith Gicheha <fwgicheh@gmail.com>
2020-10-10 16:40:41 -0700

    add creator attribute to data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@
 Project.destroy_all
 Task.destroy_all
 
-event_management = Project.create(name:"Event Management")
-design_conference = Project.create(name:"Design Conference")
+event_management = Project.find_or_create_by(name:"Event Management")
+design_conference = Project.find_or_create_by(name:"Design Conference")
 
-Task.create(title: "Finalize Budget", deadline: "Mon, 21 Oct 2020", completed: false, project_id: event_management.id)
-Task.create(title: "Confirm Sponsorship", deadline: "Mon, 28 Oct 2020", completed: true, project_id: design_conference.id)
+Task.find_or_create_by(title: "Finalize Budget", deadline: "Mon, 21 Oct 2020", creator: "Faith", completed: false, project_id: event_management.id)
+Task.find_or_create_by(title: "Confirm Sponsorship", deadline: "Mon, 28 Oct 2020", creator: "Kevin", completed: true, project_id: design_conference.id)
