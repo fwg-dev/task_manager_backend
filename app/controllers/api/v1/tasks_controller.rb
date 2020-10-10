f708afa8e2ad04ec021a36bcb23edab61267203a
Faith Gicheha <fwgicheh@gmail.com>
2020-10-10 02:48:15 -0700

    implement task index and create methods

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::TasksController < ApplicationController
+
+  def index
+    tasks = Task.all 
+    render json: tasks
+  end
+
+  def create 
+    task = task.new(task_params)
+    if task.save
+      render json: task, status: :accepted
+    else 
+      render json: {errors: task.errors.full_messages}, status: :unprocessible_entity
+    end
+  end
+end
+
+
+  end
+
+  def task_params 
+    params.require(:task).permit(:title, :deadline, :completed, :project_id)
+  end
+
+ end
